3687ec916d2a0f74acb5fcff9f428c0845510a12
caedenph <caedenperelliharris@gmail.com>
2023-07-05 16:37:21 +0100

    feat: Add version to config response

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,14 @@
 import json
 import yaml
+import requests
 
 def lambda_handler(event, context):
     with open('./config.yaml') as f:
         config = yaml.safe_load(f)
+
+
+    request = requests.get("https://api.github.com/repos/graymattermetrics/config/branches/main")
+    config['version'] = request.json()['commit']['sha']
 
     return {
         'statusCode': 200,
