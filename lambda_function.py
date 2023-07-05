5e1fd903eb22f3a766d34ba6c7ae5389b6dc1ff6
caedenph <caedenperelliharris@gmail.com>
2023-07-05 17:37:41 +0100

    feat: Take branch as a parameter if version is not passed

--- lambda_function.py
+++ lambda_function.py
@@ -6,12 +6,14 @@ import requests
 def lambda_handler(event, context):
     """
     Takes in optional paramater version from the queryStringParameters
-    which is a git commit sha. If no version is provided, the latest commit from main
-    is used.
+    which is a git commit sha. If no version is provided, you can provide a 
+    branch name and it will return the latest commit sha for that branch.
+    Defaults to main.
     """
     version = (event['queryStringParameters'] or {}).get('version', None)
     if version is None:
-        request = requests.get("https://api.github.com/repos/graymattermetrics/config/branches/main")
+        branch = (event['queryStringParameters'] or {}).get('branch', 'main')
+        request = requests.get(f"https://api.github.com/repos/graymattermetrics/config/branches/{branch}")
         version = request.json()['commit']['sha']
 
     request = requests.get(f"https://raw.githubusercontent.com/graymattermetrics/config/{version}/config.yaml")
